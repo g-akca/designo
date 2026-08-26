6d1c27ac807422369d676a0ddd6b3c8cfad94732
g-akca <gok.akca66@gmail.com>
2026-08-26 17:47:41 +0300

    Create Header and Footer components and add them to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
     <div className="text-base leading-base text-dark-grey min-h-screen bg-white flex flex-col">
+      <Header />
 
+      <main></main>
+
+      <Footer />
     </div>
   )
 }

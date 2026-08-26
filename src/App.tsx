13a85fbb5b8ed4609657c4701d2d862afbb6896a
g-akca <gok.akca66@gmail.com>
2026-08-26 17:55:26 +0300

    Add MainRouter component for routing and integrate it into App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import MainRouter from "./routers/MainRouter";
 
 function App() {
   return (
     <div className="text-base leading-base text-dark-grey min-h-screen bg-white flex flex-col">
       <Header />
 
-      <main></main>
+      <main>
+        <MainRouter />
+      </main>
 
       <Footer />
     </div>
---
+++ src/routers/MainRouter.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+
+function MainRouter() {
+  return (
+    <Routes>
+      <Route index />
+    </Routes>
+  )
+}
+
+export default MainRouter;

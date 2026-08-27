388a37a1efa29da22065dc27b3ab823527ff3625
g-akca <gok.akca66@gmail.com>
2026-08-27 13:55:16 +0300

    Implement Header component with logo and hamburger menu button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,15 @@
+import hamburgerIcon from "/assets/shared/mobile/icon-hamburger.svg";
+import darkLogo from "/assets/shared/desktop/logo-dark.png";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="h-24 px-6 flex justify-between items-center gap-5">
+      <img src={darkLogo} alt="Designo logo" className="max-w-50.5 min-w-0 shrink" />
+
+      <button type="button" aria-label="Open menu">
+        <img src={hamburgerIcon} alt="" className="w-6" />
+      </button>
+    </header>
   )
 }
 

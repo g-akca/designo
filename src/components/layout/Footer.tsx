9b1799ade1de29e920a1b7ba3d36e8bc8db6567d
g-akca <gok.akca66@gmail.com>
2026-08-29 00:26:50 +0300

    Add FooterCTA component for call-to-action section in Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,30 +5,12 @@ import pinterestIcon from "/assets/shared/desktop/icon-pinterest.svg";
 import twitterIcon from "/assets/shared/desktop/icon-twitter.svg";
 import youtubeIcon from "/assets/shared/desktop/icon-youtube.svg";
 
+import FooterCTA from "./FooterCTA";
+
 function Footer() {
   return (
     <footer className="relative py-14.5 px-6 flex flex-col gap-16.5">
-      <section 
-        className="
-          bg-peach bg-[url('/assets/shared/desktop/bg-pattern-call-to-action.svg')] bg-position-[44%_center] 
-          bg-no-repeat rounded-[15px] py-16 px-6 flex flex-col items-center gap-8 z-10
-        "
-      >
-        <div className="flex flex-col gap-4 text-center text-white">
-          <h2 className="font-medium text-[32px] leading-9">Let’s talk about your project</h2>
-
-          <p className="text-[15px] leading-6.25">
-            Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.
-          </p>
-        </div>
-
-        <button 
-          type="button" 
-          className="bg-white rounded-lg w-38 h-14 uppercase font-medium text-[15px] leading-5.5 tracking-[1px]"
-        >
-          Get in touch
-        </button>
-      </section>
+      <FooterCTA />
 
       <section className="flex flex-col gap-9 items-center text-center z-10">
         <img src={lightLogo} alt="" className="h-6.75" />
---
+++ src/components/layout/FooterCTA.tsx
@@ -0,0 +1,27 @@
+function FooterCTA() {
+  return (
+    <section 
+      className="
+        bg-peach bg-[url('/assets/shared/desktop/bg-pattern-call-to-action.svg')] bg-position-[44%_center] 
+        bg-no-repeat rounded-[15px] py-16 px-6 flex flex-col items-center gap-8 z-10
+      "
+    >
+      <div className="flex flex-col gap-4 text-center text-white">
+        <h2 className="font-medium text-[32px] leading-9">Let’s talk about your project</h2>
+
+        <p className="text-[15px] leading-6.25">
+          Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.
+        </p>
+      </div>
+
+      <button 
+        type="button" 
+        className="bg-white rounded-lg w-38 h-14 uppercase font-medium text-[15px] leading-5.5 tracking-[1px]"
+      >
+        Get in touch
+      </button>
+    </section>
+  )
+}
+
+export default FooterCTA;

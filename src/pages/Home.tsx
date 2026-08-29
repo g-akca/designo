3049b236a7d7957e42b7b9d8f9458e00165aa3bb
g-akca <gok.akca66@gmail.com>
2026-08-30 00:26:10 +0300

    Create HomeHero component and integrate it into Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import HomeHero from "../components/home/HomeHero";
+
 function Home() {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-30">
+      <HomeHero />
+    </div>
   )
 }
 

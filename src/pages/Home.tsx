5213546bf7b25b8247365ca251fee6bd8b5bac9d
g-akca <gok.akca66@gmail.com>
2026-09-02 21:19:38 +0300

    Add design cards to Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import HomeHero from "../components/home/HomeHero";
+import AppDesignCard from "../components/shared/AppDesignCard";
+import GraphicDesignCard from "../components/shared/GraphicDesignCard";
+import WebDesignCard from "../components/shared/WebDesignCard";
 
 function Home() {
   return (
     <div className="flex flex-col gap-30">
       <HomeHero />
+
+      <div className="px-6 flex flex-col gap-6">
+        <WebDesignCard />
+        <AppDesignCard />
+        <GraphicDesignCard />
+      </div>
     </div>
   )
 }

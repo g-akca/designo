6534a1dbb224b9b3dd0a6ef26992aa83db75e9af
g-akca <gok.akca66@gmail.com>
2026-09-04 00:01:17 +0300

    Create CompanyValue component and implement it in CompanyValues component

---
+++ src/components/home/CompanyValue.tsx
@@ -0,0 +1,8 @@
+function CompanyValue() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CompanyValue;
--- src/components/home/CompanyValues.tsx
+++ src/components/home/CompanyValues.tsx
@@ -1,7 +1,12 @@
+import CompanyValue from "./CompanyValue";
+
 function CompanyValues() {
   return (
-    <>
-    </>
+    <section className="flex flex-col gap-20">
+      <CompanyValue />
+      <CompanyValue />
+      <CompanyValue />
+    </section>
   )
 }
 

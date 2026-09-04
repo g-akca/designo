68f43bad201c500dafd1a03a7eee0f67da4f8608
g-akca <gok.akca66@gmail.com>
2026-09-04 18:23:55 +0300

    Refactor CompanyValues component to dynamically render CompanyValue items from an array

--- src/components/home/CompanyValues.tsx
+++ src/components/home/CompanyValues.tsx
@@ -1,11 +1,37 @@
 import CompanyValue from "./CompanyValue";
+import passionateImg from "/assets/home/desktop/illustration-passionate.svg";
+import resourcefulImg from "/assets/home/desktop/illustration-resourceful.svg";
+import friendlyImg from "/assets/home/desktop/illustration-friendly.svg";
+
+const values = [
+  {
+    img: passionateImg,
+    title: "Passionate",
+    description: "Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.",
+  },
+  {
+    img: resourcefulImg,
+    title: "Resourceful",
+    description: "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.",
+  },
+  {
+    img: friendlyImg,
+    title: "Friendly",
+    description: "We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.",
+  },
+];
 
 function CompanyValues() {
   return (
     <section className="flex flex-col gap-20">
-      <CompanyValue />
-      <CompanyValue />
-      <CompanyValue />
+      {values.map((val) => (
+        <CompanyValue
+          key={val.title}
+          img={val.img}
+          title={val.title}
+          description={val.description}
+        />
+      ))}
     </section>
   )
 }

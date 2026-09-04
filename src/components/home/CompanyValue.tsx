1a0ac3c762485ed2edd694b9b82c2201fc632673
g-akca <gok.akca66@gmail.com>
2026-09-04 22:22:02 +0300

    Implement CompanyValue component structure with props rendering

--- src/components/home/CompanyValue.tsx
+++ src/components/home/CompanyValue.tsx
@@ -6,8 +6,15 @@ type CompanyValueProps = {
 
 function CompanyValue({ img, title, description }: CompanyValueProps) {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-12 items-center">
+      <img src={img} alt="" className="w-50.5" />
+
+      <div className="flex flex-col gap-8 text-center">
+        <h2 className="font-medium text-[20px] leading-base tracking-[5px] uppercase">{title}</h2>
+
+        <p>{description}</p>
+      </div>
+    </div>
   )
 }
 

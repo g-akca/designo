ef9bfabb3f8dec70cb0f7c9bccb9ef35a022567b
g-akca <gok.akca66@gmail.com>
2026-09-05 13:34:00 +0300

    Refactor CompanyValue component to include background pattern and improve image structure

--- src/components/home/CompanyValue.tsx
+++ src/components/home/CompanyValue.tsx
@@ -1,3 +1,5 @@
+import circlePattern from "/assets/shared/desktop/bg-pattern-small-circle.svg";
+
 type CompanyValueProps = {
   img: string;
   patternStyle?: string;
@@ -8,7 +10,10 @@ type CompanyValueProps = {
 function CompanyValue({ img, patternStyle, title, description }: CompanyValueProps) {
   return (
     <div className="flex flex-col gap-12 items-center">
-      <img src={img} alt="" className="w-50.5" />
+      <div className="relative w-50.5">
+        <img src={circlePattern} alt="" className={`absolute w-full ${patternStyle ?? ""}`} />
+        <img src={img} alt="" className="relative z-10 max-w-50.25" />
+      </div>
 
       <div className="flex flex-col gap-8 text-center">
         <h2 className="font-medium text-[20px] leading-base tracking-[5px] uppercase">{title}</h2>

c77c332ddda4ea34444607f9c5d412f39728161e
g-akca <gok.akca66@gmail.com>
2026-09-08 11:26:49 +0300

    Add Menu component with navigation links for company, locations, and contact

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router";
+
 function Menu() {
   return (
-    <>
-    </>
+    <nav className="bg-black px-6 py-12">
+      <ul className="flex flex-col gap-8 text-[24px] leading-6.25 tracking-[2px] text-white uppercase">
+        <li>
+          <NavLink to="about">Our Company</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="locations">Locations</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 

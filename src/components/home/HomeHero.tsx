fcc4502929c641d47e43f062aa9339aaeba2402d
g-akca <gok.akca66@gmail.com>
2026-09-08 23:10:31 +0300

    Add tablet responsive styles to HomeHero component

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -5,14 +5,17 @@ function HomeHero() {
     <section 
       className="
         bg-peach bg-[url('/assets/home/desktop/bg-pattern-hero-home.svg')] bg-left 
-        bg-no-repeat px-6 pt-20 pb-108 relative overflow-hidden
+        bg-no-repeat px-6 pt-20 pb-108 relative overflow-hidden tablet:rounded-[15px]
+        tablet:bg-position-[170px_center] tablet:pt-15 tablet:pb-114.5
       "
     >
-      <div className="flex flex-col items-center gap-6">
-        <div className="flex flex-col gap-7 text-center text-white">
-          <h2 className="font-medium text-[32px] leading-9">Award-winning custom designs and digital branding solutions</h2>
+      <div className="flex flex-col items-center gap-6 tablet:gap-4.75">
+        <div className="flex flex-col items-center gap-7 text-center text-white">
+          <h2 className="max-w-145 font-medium text-[32px] leading-9 tablet:text-[48px] tablet:leading-12">
+            Award-winning custom designs and digital branding solutions
+          </h2>
 
-          <p className="text-[15px] leading-6.25">
+          <p className="max-w-111.25 text-[15px] leading-6.25 tablet:text-base tablet:leading-base">
             With over 10 years in the industry, we are experienced in creating fully responsive websites, 
             app design, and engaging brand experiences. Find out more about our services.
           </p>
@@ -26,7 +29,11 @@ function HomeHero() {
         </button>
       </div>
 
-      <img src={heroPhone} alt="" className="absolute -bottom-89.5 left-1/2 -translate-x-1/2 max-w-none w-150" />
+      <img 
+        src={heroPhone} 
+        alt="" 
+        className="absolute -bottom-89.5 left-1/2 -translate-x-1/2 max-w-none w-150 tablet:-bottom-89 tablet:w-156" 
+      />
     </section>
   )
 }

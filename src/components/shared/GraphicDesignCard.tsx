e5c2f6dd0e5d96a43c4af3953a9ac99a63843d92
g-akca <gok.akca66@gmail.com>
2026-09-08 21:42:16 +0300

    Update card components for consistent padding and improve layout in Home page

--- src/components/shared/GraphicDesignCard.tsx
+++ src/components/shared/GraphicDesignCard.tsx
@@ -6,7 +6,7 @@ function GraphicDesignCard() {
     <Link 
       to="/graphic-design" 
       className="
-        relative h-62.5 rounded-[15px] bg-[url('/assets/home/mobile/image-graphic-design.jpg')] 
+        relative p-6 rounded-[15px] bg-[url('/assets/home/mobile/image-graphic-design.jpg')] 
         bg-center bg-cover bg-no-repeat overflow-hidden flex flex-col 
         justify-center items-center gap-3 text-center text-white uppercase font-medium
       "
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ import WebDesignCard from "../components/shared/WebDesignCard";
 
 function Home() {
   return (
-    <div className="flex flex-col gap-30">
+    <div className="flex flex-col gap-30 tablet:px-9.75">
       <HomeHero />
 
-      <div className="px-6 flex flex-col gap-30">
-        <div className="flex flex-col gap-6">
+      <div className="px-6 flex flex-col gap-30 tablet:px-0">
+        <div className="grid grid-rows-[repeat(3,250px)] gap-6 tablet:grid-rows-[repeat(3,200px)] tablet:gap-6.25">
           <WebDesignCard />
           <AppDesignCard />
           <GraphicDesignCard />

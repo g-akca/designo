c5a21b17c46f252da4aa67b5829f034f17011113
g-akca <gok.akca66@gmail.com>
2026-09-09 20:31:59 +0300

    Enhance tablet responsiveness for design cards with updated text sizes and spacing

--- src/components/shared/WebDesignCard.tsx
+++ src/components/shared/WebDesignCard.tsx
@@ -9,13 +9,16 @@ function WebDesignCard() {
         relative p-6 rounded-[15px] bg-[url('/assets/home/mobile/image-web-design.jpg')] 
         bg-center bg-cover bg-no-repeat overflow-hidden flex flex-col
         justify-center items-center gap-3 text-center text-white uppercase font-medium
+        tablet:bg-[url('/assets/home/tablet/image-web-design.jpg')] tablet:gap-6.5
       "
     >
       <div className="absolute inset-0 bg-[#000000]/55" />
 
-      <h2 className="z-10 text-[28px] leading-9 tracking-[1.4px]">Web Design</h2>
+      <h2 className="z-10 text-[28px] leading-9 tracking-[1.4px] tablet:text-[40px] tablet:leading-12 tablet:tracking-[2px]">
+        Web Design
+      </h2>
 
-      <div className="z-10 flex items-center gap-4">
+      <div className="z-10 flex items-center gap-4 tablet:gap-5.25">
         <p className="text-[15px] leading-5.5 tracking-[5px]">View projects</p>
 
         <img src={arrowIcon} alt="" />

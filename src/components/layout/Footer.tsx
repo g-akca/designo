dc250e5a57fa99edb8cd25cb5f9312419157cad6
g-akca <gok.akca66@gmail.com>
2026-09-10 00:14:38 +0300

    Add tablet responsiveness to Footer component with updated padding and height

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,12 +3,12 @@ import FooterInfo from "./FooterInfo";
 
 function Footer() {
   return (
-    <footer className="relative pb-14.5 px-6 flex flex-col gap-16.5">
+    <footer className="relative pb-14.5 px-6 flex flex-col gap-16.5 tablet:pb-20 tablet:px-9.75 tablet:gap-20">
       <FooterCTA />
 
       <FooterInfo />
 
-      <div className="absolute z-0 bottom-0 inset-x-0 bg-black h-203.75" />
+      <div className="absolute z-0 bottom-0 inset-x-0 bg-black h-203.75 tablet:h-105.75" />
     </footer>
   )
 }

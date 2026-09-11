d7562cf3ceaccd375aec74a4dd5d54ab0ae85463
g-akca <gok.akca66@gmail.com>
2026-09-11 13:30:08 +0300

    Refactor layout structure in FooterInfo component for improved responsiveness

--- src/components/layout/FooterInfo.tsx
+++ src/components/layout/FooterInfo.tsx
@@ -20,25 +20,27 @@ function FooterInfo() {
       <FooterLinks className="tablet:hidden" />
 
       <div className="flex flex-col gap-9 items-center tablet:flex-row tablet:items-end tablet:justify-between">
-        <address>
-          <p className="text-white/50 not-italic">
-            <strong>Designo Central Office</strong>
-            <br />
-            3886 Wellington Street
-            <br />
-            Toronto, Ontario M9C 3J5
-          </p>
-        </address>
+        <div className="flex flex-col gap-9 items-center tablet:flex-row tablet:gap-14.5">
+          <address>
+            <p className="text-white/50 not-italic">
+              <strong>Designo Central Office</strong>
+              <br />
+              3886 Wellington Street
+              <br />
+              Toronto, Ontario M9C 3J5
+            </p>
+          </address>
 
-        <address>
-          <p className="text-white/50 not-italic">
-            <strong>Contact Us (Central Office)</strong>
-            <br />
-            P : +1 253-863-8967
-            <br />
-            M : contact@designo.co
-          </p>
-        </address>
+          <address>
+            <p className="text-white/50 not-italic">
+              <strong>Contact Us (Central Office)</strong>
+              <br />
+              P : +1 253-863-8967
+              <br />
+              M : contact@designo.co
+            </p>
+          </address>
+        </div>
 
         <nav className="flex gap-4 items-center">
           <a href="#" aria-label="Facebook">

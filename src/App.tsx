6e85c155b41a7bdf836d6e5ac6f098e99549e6dc
g-akca <gok.akca66@gmail.com>
2026-09-11 17:14:27 +0300

    Enhance layout in App component for desktop responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className="text-base leading-base text-dark-grey min-h-screen bg-white flex flex-col justify-between gap-24 tablet:gap-16">
+    <div 
+      className="
+        text-base leading-base text-dark-grey min-h-screen bg-white flex flex-col justify-between gap-24 tablet:gap-16 
+        desktop:gap-40 desktop:bg-[url('/assets/shared/desktop/bg-pattern-leaf.svg')] desktop:bg-position-[0_475px] desktop:bg-no-repeat
+      "
+    >
       {isMenuOpen && (
         <div className="fixed inset-0 z-40 bg-black/50 tablet:hidden" />
       )}
 
-      <div className="flex flex-col gap-24 tablet:gap-16 tablet:pt-16 tablet:px-9.75">
+      <div className="flex flex-col gap-24 tablet:gap-16 tablet:pt-16 tablet:px-9.75 desktop:px-0 desktop:self-center desktop:max-w-277.5">
         <Header 
           isMenuOpen={isMenuOpen} 
           openMenu={() => setIsMenuOpen(true)}

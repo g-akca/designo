6c149e19489e2b63e17e80bdb2c1ba392e5ae0fd
g-akca <gok.akca66@gmail.com>
2026-09-14 15:50:42 +0300

    Refactor Footer component for improved layout and styling consistency, add desktop responsive styles

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,19 @@
 import FooterCTA from "./FooterCTA";
 import FooterInfo from "./FooterInfo";
+import leafPattern from "/assets/shared/desktop/bg-pattern-leaf.svg";
 
 function Footer() {
   return (
-    <footer className="relative pb-14.5 px-6 flex flex-col gap-16.5 tablet:pb-20 tablet:px-9.75 tablet:gap-20">
-      <FooterCTA />
+    <footer className="relative pb-14.5 px-6 flex justify-center tablet:pb-20 tablet:px-9.75 desktop:pb-18">
+      <img className="absolute right-0 -top-113 rotate-180" src={leafPattern} alt="" />
 
-      <FooterInfo />
+      <div className="grow max-w-277.5 flex flex-col gap-16.5 tablet:gap-20 desktop:gap-18">
+        <FooterCTA />
 
-      <div className="absolute z-0 bottom-0 inset-x-0 bg-black h-203.75 tablet:h-105.75" />
+        <FooterInfo />
+      </div>
+
+      <div className="absolute bottom-0 inset-x-0 bg-black h-203.75 tablet:h-105.75" />
     </footer>
   )
 }

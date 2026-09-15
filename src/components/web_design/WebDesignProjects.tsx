fbc7bdbc64079c3306612af826899ad13dad27e8
g-akca <gok.akca66@gmail.com>
2026-09-15 19:40:18 +0300

    Add WebDesign page components and structure layout with HeroSection and WebDesignProjects

---
+++ src/components/web_design/WebDesignProjects.tsx
@@ -0,0 +1,8 @@
+function WebDesignProjects() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default WebDesignProjects;
--- src/pages/WebDesign.tsx
+++ src/pages/WebDesign.tsx
@@ -1,7 +1,23 @@
+import AppDesignCard from "../components/shared/AppDesignCard";
+import GraphicDesignCard from "../components/shared/GraphicDesignCard";
+import HeroSection from "../components/web_design/HeroSection";
+import WebDesignProjects from "../components/web_design/WebDesignProjects";
+
 function WebDesign() {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-24">
+      <HeroSection />
+
+      <div className="px-6 flex flex-col gap-24">
+        <WebDesignProjects />
+
+        <div className="grid grid-rows-[repeat(2,250px)] gap-6">
+          <AppDesignCard />
+
+          <GraphicDesignCard />
+        </div>
+      </div>
+    </div>
   )
 }
 

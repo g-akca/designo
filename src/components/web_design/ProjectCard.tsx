0c6c1a7d85dd07cf210b4a671f82f75ed30b60a3
g-akca <gok.akca66@gmail.com>
2026-09-15 20:42:27 +0300

    Implement ProjectCard component layout and structure

--- src/components/web_design/ProjectCard.tsx
+++ src/components/web_design/ProjectCard.tsx
@@ -6,8 +6,15 @@ type ProjectCardProps = {
 
 function ProjectCard({ name, description, image }: ProjectCardProps) {
   return (
-    <>
-    </>
+    <div className="flex flex-col bg-[#FDF3F0] rounded-[15px] overflow-hidden">
+      <img src={image} alt={name} className="h-80 object-cover object-top" />
+
+      <div className="px-7.5 py-8 flex flex-col gap-4 text-center">
+        <h2 className="text-peach text-[20px] leading-base tracking-[5px] font-medium uppercase">{name}</h2>
+
+        <p>{description}</p>
+      </div>
+    </div>
   )
 }
 

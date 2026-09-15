61cc88f2264905ba1922d01f4b735d84bcd1fe21
g-akca <gok.akca66@gmail.com>
2026-09-15 20:54:32 +0300

    Enhance HeroSection and WebDesign layout for tablet responsiveness

--- src/components/web_design/HeroSection.tsx
+++ src/components/web_design/HeroSection.tsx
@@ -2,13 +2,18 @@ function HeroSection() {
   return (
     <section 
       className="
-        min-h-80 bg-[url('/assets/web-design/desktop/bg-pattern-intro-web.svg')] bg-top-right bg-no-repeat 
-        bg-peach p-6 flex flex-col justify-center items-center gap-6 text-white text-center
+        min-h-80 bg-[url('/assets/web-design/desktop/bg-pattern-intro-web.svg')] bg-top-right 
+        bg-no-repeat bg-peach p-6 flex flex-col justify-center items-center gap-6 text-white 
+        text-center tablet:min-h-63 tablet:bg-position-[-20px_center] tablet:rounded-[15px]
       "
     >
-      <h1 className="font-medium text-[32px] leading-9">Web Design</h1>
+      <h1 className="font-medium text-[32px] leading-9 tablet:text-[48px] tablet:leading-12">
+        Web Design
+      </h1>
 
-      <p className="text-[15px] leading-6.25">We build websites that serve as powerful marketing tools and bring memorable brand experiences.</p>
+      <p className="text-[15px] leading-6.25 tablet:text-base tablet:leading-base tablet:max-w-100">
+        We build websites that serve as powerful marketing tools and bring memorable brand experiences.
+      </p>
     </section>
   )
 }
--- src/pages/WebDesign.tsx
+++ src/pages/WebDesign.tsx
@@ -5,13 +5,13 @@ import ProjectsSection from "../components/web_design/ProjectsSection";
 
 function WebDesign() {
   return (
-    <div className="flex flex-col gap-24">
+    <div className="flex flex-col gap-24 tablet:gap-30">
       <HeroSection />
 
-      <div className="px-6 flex flex-col gap-24">
+      <div className="px-6 flex flex-col gap-24 tablet:gap-30">
         <ProjectsSection />
 
-        <div className="grid grid-rows-[repeat(2,250px)] gap-6">
+        <div className="grid grid-rows-[repeat(2,250px)] gap-6 tablet:grid-rows-[repeat(2,200px)]">
           <AppDesignCard />
 
           <GraphicDesignCard />

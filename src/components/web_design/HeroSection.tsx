b649f612261f98230f7471c8c9c61b81755d2bcb
g-akca <gok.akca66@gmail.com>
2026-09-15 20:11:29 +0300

    Add HeroSection component with layout and content for Web Design page

--- src/components/web_design/HeroSection.tsx
+++ src/components/web_design/HeroSection.tsx
@@ -1,7 +1,15 @@
 function HeroSection() {
   return (
-    <>
-    </>
+    <section 
+      className="
+        min-h-80 bg-[url('/assets/web-design/desktop/bg-pattern-intro-web.svg')] bg-top-right bg-no-repeat 
+        bg-peach p-6 flex flex-col justify-center items-center gap-6 text-white text-center
+      "
+    >
+      <h1 className="font-medium text-[32px] leading-9">Web Design</h1>
+
+      <p className="text-[15px] leading-6.25">We build websites that serve as powerful marketing tools and bring memorable brand experiences.</p>
+    </section>
   )
 }
 

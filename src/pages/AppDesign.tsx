e13a8799e376603afe62142bebfddd7879371464
g-akca <gok.akca66@gmail.com>
2026-09-16 21:59:13 +0300

    Update AppDesign layout: enhance responsiveness with tablet-specific styles for gaps and padding

--- src/pages/AppDesign.tsx
+++ src/pages/AppDesign.tsx
@@ -39,13 +39,13 @@ const projects = [
 
 function AppDesign() {
   return (
-    <div className="flex flex-col gap-24">
+    <div className="flex flex-col gap-24 tablet:gap-30">
       <HeroSection heading="App Design" intro="Our mobile designs bring intuitive digital solutions to your customers right at their fingertips." />
 
-      <div className="px-6 flex flex-col gap-24">
+      <div className="px-6 flex flex-col gap-24 tablet:px-0 tablet:gap-30">
         <ProjectsSection projects={projects} />
 
-        <div className="grid grid-rows-[repeat(2,250px)] gap-6">
+        <div className="grid grid-rows-[repeat(2,250px)] gap-6 tablet:grid-rows-[repeat(2,200px)]">
           <WebDesignCard />
 
           <GraphicDesignCard />

93b674bf5a01d9261b2c7a5f657280ddd5a5a2e0
g-akca <gok.akca66@gmail.com>
2026-09-16 22:16:59 +0300

    Add GraphicDesign page with HeroSection and ProjectsSection components

--- src/pages/GraphicDesign.tsx
+++ src/pages/GraphicDesign.tsx
@@ -1,7 +1,45 @@
+import WebDesignCard from "../components/shared/WebDesignCard";
+import AppDesignCard from "../components/shared/AppDesignCard";
+import HeroSection from "../components/shared/HeroSection";
+import ProjectsSection from "../components/shared/ProjectsSection";
+
+import changeImg from "/assets/graphic-design/desktop/image-change.jpg";
+import boxedWaterImg from "/assets/graphic-design/desktop/image-boxed-water.jpg";
+import scienceImg from "/assets/graphic-design/desktop/image-science.jpg";
+
+const projects = [
+  {
+    name: "Tim Brown",
+    description: "A book cover designed for Tim Brown’s new release, ‘Change’",
+    image: changeImg,
+  },
+  {
+    name: "Boxed Water",
+    description: "A simple packaging concept made for Boxed Water",
+    image: boxedWaterImg,
+  },
+  {
+    name: "Science!",
+    description: "A poster made in collaboration with the Federal Art Project",
+    image: scienceImg,
+  },
+];
+
 function GraphicDesign() {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-24">
+      <HeroSection background="graphic" heading="Graphic Design" intro="We deliver eye-catching branding materials that are tailored to meet your business objectives." />
+
+      <div className="px-6 flex flex-col gap-24">
+        <ProjectsSection projects={projects} />
+
+        <div className="grid grid-rows-[repeat(2,250px)] gap-6">
+          <AppDesignCard />
+
+          <WebDesignCard />
+        </div>
+      </div>
+    </div>
   )
 }
 

0e3873ac6173b028c74f29f4b5b6d296a7a7219a
g-akca <gok.akca66@gmail.com>
2026-09-16 22:00:09 +0300

    Update AppDesign layout: enhance responsiveness with desktop-specific styles for gaps and grid layout

--- src/pages/AppDesign.tsx
+++ src/pages/AppDesign.tsx
@@ -39,13 +39,13 @@ const projects = [
 
 function AppDesign() {
   return (
-    <div className="flex flex-col gap-24 tablet:gap-30">
+    <div className="flex flex-col gap-24 tablet:gap-30 desktop:gap-40">
       <HeroSection heading="App Design" intro="Our mobile designs bring intuitive digital solutions to your customers right at their fingertips." />
 
-      <div className="px-6 flex flex-col gap-24 tablet:px-0 tablet:gap-30">
+      <div className="px-6 flex flex-col gap-24 tablet:px-0 tablet:gap-30 desktop:gap-40">
         <ProjectsSection projects={projects} />
 
-        <div className="grid grid-rows-[repeat(2,250px)] gap-6 tablet:grid-rows-[repeat(2,200px)]">
+        <div className="grid grid-rows-[repeat(2,250px)] gap-6 tablet:grid-rows-[repeat(2,200px)] desktop:grid-rows-[308px] desktop:grid-cols-2 desktop:gap-7.5">
           <WebDesignCard />
 
           <GraphicDesignCard />

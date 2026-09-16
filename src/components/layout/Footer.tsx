d595c7d0e0bc21b578d6368cb880deea8a398eda
g-akca <gok.akca66@gmail.com>
2026-09-16 19:56:00 +0300

    Add visibility of leaf pattern in Footer component for only desktop layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import leafPattern from "/assets/shared/desktop/bg-pattern-leaf.svg";
 function Footer() {
   return (
     <footer className="relative pb-14.5 px-6 flex justify-center tablet:pb-20 tablet:px-9.75 desktop:pb-18">
-      <img className="absolute right-0 -top-113 rotate-180" src={leafPattern} alt="" />
+      <img className="hidden desktop:block absolute right-0 -top-113 rotate-180" src={leafPattern} alt="" />
 
       <div className="grow max-w-277.5 flex flex-col gap-16.5 tablet:gap-20 desktop:gap-18">
         <FooterCTA />

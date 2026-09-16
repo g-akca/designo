989e118f375c6d08a462d6d9fc0e4bc096c5e44d
g-akca <gok.akca66@gmail.com>
2026-09-16 21:58:08 +0300

    Update AppDesign page: define projects array and pass it to ProjectsSection component

--- src/pages/AppDesign.tsx
+++ src/pages/AppDesign.tsx
@@ -3,13 +3,47 @@ import GraphicDesignCard from "../components/shared/GraphicDesignCard";
 import HeroSection from "../components/shared/HeroSection";
 import ProjectsSection from "../components/shared/ProjectsSection";
 
+import airfilterImg from "/assets/app-design/desktop/image-airfilter.jpg";
+import eyecamImg from "/assets/app-design/desktop/image-eyecam.jpg";
+import faceitImg from "/assets/app-design/desktop/image-faceit.jpg";
+import todoImg from "/assets/app-design/desktop/image-todo.jpg";
+import loopstudiosImg from "/assets/app-design/desktop/image-loopstudios.jpg";
+
+const projects = [
+  {
+    name: "Airfilter",
+    description: "Solving the problem of poor indoor air quality by filtering the air",
+    image: airfilterImg,
+  },
+  {
+    name: "Eyecam",
+    description: "Product that lets you edit your favorite photos and videos at any time",
+    image: eyecamImg,
+  },
+  {
+    name: "Faceit",
+    description: "Get to meet your favorite internet superstar with the faceit app",
+    image: faceitImg,
+  },
+  {
+    name: "Todo",
+    description: "A todo app that features cloud sync with light and dark mode",
+    image: todoImg,
+  },
+  {
+    name: "Loopstudios",
+    description: "A VR experience app made for Loopstudios",
+    image: loopstudiosImg,
+  },
+];
+
 function AppDesign() {
   return (
     <div className="flex flex-col gap-24">
       <HeroSection heading="App Design" intro="Our mobile designs bring intuitive digital solutions to your customers right at their fingertips." />
 
       <div className="px-6 flex flex-col gap-24">
-        <ProjectsSection />
+        <ProjectsSection projects={projects} />
 
         <div className="grid grid-rows-[repeat(2,250px)] gap-6">
           <WebDesignCard />

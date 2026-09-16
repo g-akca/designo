b42fe365c72f8b9485b57b83ba2cafed1377e829
g-akca <gok.akca66@gmail.com>
2026-09-16 20:09:22 +0300

    Enhance ProjectCard component with hover effects for title and description

--- src/components/web_design/ProjectCard.tsx
+++ src/components/web_design/ProjectCard.tsx
@@ -8,16 +8,17 @@ function ProjectCard({ name, description, image }: ProjectCardProps) {
   return (
     <div 
       className="
-        flex flex-col bg-[#FDF3F0] rounded-[15px] overflow-hidden tablet:min-h-77.5 
-        tablet:grid tablet:grid-cols-[339px_auto] tablet:items-center desktop:flex
+        group flex flex-col bg-[#FDF3F0] rounded-[15px] overflow-hidden tablet:min-h-77.5 
+        tablet:grid tablet:grid-cols-[339px_auto] tablet:items-center desktop:flex 
+        hover:bg-peach transition-all duration-300
       "
     >
       <img src={image} alt={name} className="h-80 object-cover object-top tablet:h-auto" />
 
       <div className="px-7.5 py-8 flex flex-col gap-4 text-center tablet:px-8 tablet:gap-3.75 desktop:gap-4">
-        <h2 className="text-peach text-[20px] leading-base tracking-[5px] font-medium uppercase">{name}</h2>
+        <h2 className="text-peach text-[20px] leading-base tracking-[5px] font-medium uppercase group-hover:text-white transition-all duration-300">{name}</h2>
 
-        <p>{description}</p>
+        <p className="group-hover:text-white transition-all duration-300">{description}</p>
       </div>
     </div>
   )

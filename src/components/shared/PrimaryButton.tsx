81945116f86613b71b296cff7dd6e6656b677a79
g-akca <gok.akca66@gmail.com>
2026-09-17 19:22:10 +0300

    Refactor PrimaryButton component to accept color prop for dynamic styling

--- src/components/shared/PrimaryButton.tsx
+++ src/components/shared/PrimaryButton.tsx
@@ -1,15 +1,17 @@
 type PrimaryButtonProps = {
   children: React.ReactNode;
+  color?: "white" | "peach";
 };
 
-function PrimaryButton({ children }: PrimaryButtonProps) {
+function PrimaryButton({ children, color = "white" }: PrimaryButtonProps) {
   return (
     <button
       type="button"
-      className="
-        bg-white rounded-lg min-w-38 min-h-14 uppercase font-medium text-[15px] leading-5.5
-        tracking-[1px] hover:bg-light-peach hover:text-white transition-all cursor-pointer
-      "
+      className={`
+        rounded-lg min-w-38 min-h-14 uppercase font-medium text-[15px] leading-5.5
+        tracking-[1px] transition-all cursor-pointer hover:bg-light-peach
+        ${color === "white" ? "bg-white hover:text-white" : "bg-peach text-white"}
+      `}
     >
       {children}
     </button>

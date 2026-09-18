29bf4acc42c303a358d8e70680d596f4e3b5988e
g-akca <gok.akca66@gmail.com>
2026-09-18 16:13:13 +0300

    Add LocationsSection and LocationItem components for location display

---
+++ src/components/shared/LocationItem.tsx
@@ -0,0 +1,13 @@
+type LocationItemProps = {
+  name: string;
+  image: string;
+};
+
+function LocationItem({ name, image }: LocationItemProps) {
+  return (
+    <>
+    </>
+  )
+}
+
+export default LocationItem;
--- src/components/shared/LocationsSection.tsx
+++ src/components/shared/LocationsSection.tsx
@@ -1,7 +1,34 @@
+import LocationItem from "./LocationItem";
+import canadaImg from "/assets/shared/desktop/illustration-canada.svg";
+import australiaImg from "/assets/shared/desktop/illustration-australia.svg";
+import ukImg from "/assets/shared/desktop/illustration-united-kingdom.svg";
+
+const locations = [
+  {
+    name: "Canada",
+    image: canadaImg,
+  },
+  {
+    name: "Australia",
+    image: australiaImg,
+  },
+  {
+    name: "United Kingdom",
+    image: ukImg,
+  },
+];
+
 function LocationsSection() {
   return (
-    <>
-    </>
+    <section className="flex flex-col gap-12 items-center">
+      {locations.map((loc) => (
+        <LocationItem 
+          key={loc.name}
+          name={loc.name}
+          image={loc.image}
+        />
+      ))}
+    </section>
   )
 }
 

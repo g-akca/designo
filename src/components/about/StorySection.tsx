c135645921a221e5ca50c5e49541a0c30181640c
g-akca <gok.akca66@gmail.com>
2026-09-24 09:02:03 +0300

    Refine layout and styling for StorySection component

--- src/components/about/StorySection.tsx
+++ src/components/about/StorySection.tsx
@@ -21,17 +21,17 @@ function StorySection({ heading, story, image, tabletImage, desktopImage, isReve
         desktop:bg-bottom desktop:min-h-160 desktop:gap-23.5 ${isReversed ? "desktop:flex-row-reverse" : "desktop:flex-row"}
       `}
     >
-      <picture>
+      <picture className="desktop:w-119 desktop:shrink-0">
         <source media="(min-width: 1440px)" srcSet={desktopImage} />
         <source media="(min-width: 768px)" srcSet={tabletImage} />
-        <img src={image} alt="" className="h-80 w-full object-cover desktop:h-full desktop:w-119" />
+        <img src={image} alt="" className="h-80 w-full object-cover desktop:h-full" />
       </picture>
 
       <div 
         className={`
           px-6 py-12 min-h-136.25 max-w-143 self-center flex flex-col justify-center 
           gap-6 text-center tablet:py-17 tablet:px-0 tablet:min-h-104 desktop:text-start
-          desktop:min-h-auto ${isReversed ? "desktop:pl-32" : "desktop:pr-32"}
+          desktop:min-h-auto ${isReversed ? "desktop:pl-23.75" : "desktop:pr-24"}
         `}
       >
         <h2 className="text-[32px] leading-9 font-medium text-peach tablet:text-[40px] tablet:leading-12">{heading}</h2>

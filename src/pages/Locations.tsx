912ed8ae1b32fd1e31ed45c0eb6e4abacdb8f532
g-akca <gok.akca66@gmail.com>
2026-09-26 15:57:01 +0300

    Add Locations page and implement LocationCard component

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,7 +1,14 @@
+import LocationCard from "../components/locations/LocationCard";
+
 function Locations() {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-30">
+      <div className="flex flex-col gap-10">
+        <LocationCard />
+        <LocationCard />
+        <LocationCard />
+      </div>
+    </div>
   )
 }
 
